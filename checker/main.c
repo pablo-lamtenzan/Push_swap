f69f98c396c1c9a6708cf8e07be901ce4dc3230e
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-03-09 19:52:27 +0100

    checker done ?

--- checker/main.c
+++ checker/main.c
@@ -6,17 +6,72 @@
 /*   By: pablo <pablo@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 18:01:18 by pablo             #+#    #+#             */
-/*   Updated: 2021/03/09 19:08:48 by pablo            ###   ########lyon.fr   */
+/*   Updated: 2021/03/09 19:51:53 by pablo            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <stack.h>
+# include <unistd.h>
+# include <limits.h>
 
-int main(int ac, const char *av)
+static void checker(t_stack *const a, t_stack *const b)
 {
-	// Read with gnl each line
-	// Repeat the same operations
-	// Check if is sorted -> (print OK or KO)
-	// Easy as fck
-	// NOTE: Cltr + D
+	static const char ok[] = "OK";
+	static const char ko[] = "KO";
+	long cmp;
+	size_t it;
+	
+	if (b->lenght != 0ul)
+	{
+		write(STDOUT_FILENO, ko, sizeof(ko));
+		return ;
+	}
+	cmp = LONG_MAX;
+	it = -1;
+	while (++it < a->lenght)
+	{
+		if (a->data[it] >= cmp)
+		{
+			write(STDOUT_FILENO, ko, sizeof(ko));
+			return ;
+		}
+		cmp = a->data[it];
+	}
+	write (STDOUT_FILENO, ok, sizeof(ok));
+}
+
+static void execute_operation(t_stack *const a, t_stack *const b, char* line)
+{
+	size_t it;
+
+	it = 0ul;
+	// This is a security exploit, must securize it
+	while (strcmp(line, char_operation(it)))
+		it++;
+	operation(it)(a, b);
+}
+
+// TO DO: Note ctrl+D
+// include gnl and test
+// include / change strcmp
+int main(int ac, const char **av)
+{
+	t_stack a;
+	t_stack b;
+	char *line;
+
+	a = (t_stack){ .capacity=(size_t)(ac-1), .lenght=(size_t)(ac-1) };
+	b = (t_stack){ .capacity=(size_t)(ac-1), .lenght=0ul };
+	stack_init(&a, av);
+	stack_init(&b, av);
+	while (get_next_line(STDIN_FILENO, line) > 0)
+	{
+		execute_operation(&a, &b, line);
+		free(line);
+	}
+	free(line);
+	checker(&a, &b);
+	stack_clear(&a);
+	stack_clear(&b);
+	return (EXIT_SUCCESS);
 }
