0eac61f15accc9c24ec6e40b3025b485b5130a8b
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-03-09 19:14:19 +0100

    checker initial commit

---
+++ checker/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablo <pablo@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 18:01:18 by pablo             #+#    #+#             */
+/*   Updated: 2021/03/09 19:08:48 by pablo            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stack.h>
+
+int main(int ac, const char *av)
+{
+	// Read with gnl each line
+	// Repeat the same operations
+	// Check if is sorted -> (print OK or KO)
+	// Easy as fck
+	// NOTE: Cltr + D
+}
